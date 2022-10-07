1d8d5e5a85afc4c2ddb9a631ea9c917d63472c1e
ISHITSUKA Hikaru <ishitsuka.hikaru@aist.go.jp>
2022-10-07 14:42:21 +0900

    update tracker scripts

--- track.sh
+++ track.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# sources=data/2022_FukushimaRTF_HumanActionDataset_20220628/4K_mp4/5月10日無人-日常/
-# sources=data/2022_FukushimaRTF_HumanActionDataset_20220628/4K_mp4/パターンA/
 sources=$1
 height=2160
 width=3840
 yolo_weights=weights/yolov5x6.pt
 strong_sort_weights=weights/osnet_ain_x1_0_msmt17.pt
-classes="0 1 2"  # 0: person, 1: bicycle, 2: car, ... (e.g., COCO128)
+classes="0"  # 0: person, 1: bicycle, 2: car, ... (e.g., COCO128)
 name=$(basename ${yolo_weights%.*})-$(basename ${strong_sort_weights%.*})-${width}x${height}
 device=0,1
 
@@ -20,6 +18,7 @@ do
     	   --classes $classes \
     	   --save-txt \
     	   --save-vid \
+	   --save-crop \
     	   --name $name \
     	   --device $device \
     	   --exist-ok
--- tracks.sh
+++ tracks.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 sources_list=(
+    data/2022_FukushimaRTF_HumanActionDataset_20220628/4K_mp4/パターンA/
     data/2022_FukushimaRTF_HumanActionDataset_20220628/4K_mp4/パターンB/
     data/2022_FukushimaRTF_HumanActionDataset_20220628/4K_mp4/パターンC/
     data/2022_FukushimaRTF_HumanActionDataset_20220628/4K_mp4/パターンD/
     data/2022_FukushimaRTF_HumanActionDataset_20220628/4K_mp4/パターンE/
     data/2022_FukushimaRTF_HumanActionDataset_20220628/4K_mp4/パターンF/
+    data/2022_FukushimaRTF_HumanActionDataset_20220628/4K_mp4/5月10日無人-日常/
     data/2022_FukushimaRTF_HumanActionDataset_20220628/4K_mp4/5月11日無人-日常/
 )
 
